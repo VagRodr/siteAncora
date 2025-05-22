7eece22cff41557e5df2ff2984452f8f00e84e20
Vagner <vagnerlrg@gmail.com>
2025-05-21 21:29:36 -0300

    Alteração imagens banner Home e Carrossel Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,10 @@ import Carousel from '../../components/Carousel/Carousel';
 import styles from './Home.module.css';
 import NextPage from '../../components/NextPage/NextPage';
 // Essas imagens são exemplos e nao são svg. 
-import despachadoriaImg from '../../assets/images/despachadoria.png';
-import adesivosImg from '../../assets/images/adesivos.png';
-import licenciamentoImg from '../../assets/images/licenciamento.png';
-import Banner from '../../assets/images/banner_drone.png'
+import despachadoriaImg from '../../assets/images/carrosselDespachadoria.svg';
+import adesivosImg from '../../assets/images/carrosselAdesivo.svg';
+import licenciamentoImg from '../../assets/images/carrosselLicenciamento.svg';
+import Banner from '../../assets/images/banner_Home.png'
 const services = [
   {
     title: 'Despachadoria Marítima',
@@ -32,7 +32,7 @@ const Home = () => {
         </figure>
         <article className={styles.intro}>
         
-          <h1>Olá Navegantes, bem-vindos à Âncora Despachadoria Marítima.</h1>
+          <h1>Olá Navegantes, bem-vindos à Âncora Despachadoria Marítima</h1>
           <p>
             Com mais de 30 anos de atuação no mercado náutico, oferecemos soluções inteligentes em despachadoria marítima, sinalização de embarcações com adesivos náuticos e licenciamento ambiental.
           </p>
